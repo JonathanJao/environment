e9ff4c10858092c8fdc42584815ca359d24b9c3b
Jonathan Jao <fondyjao@gmail.com>
2020-06-18 02:52:42 -0500

    Add bash_profile.sh

---
+++ bash_profile.sh
@@ -0,0 +1,15 @@
+alias k=kubectl
+# complete -F __start_kubectl k
+
+alias g="git"
+alias gs="git status"
+alias gsh="git show"
+alias ga="git add"
+alias gc="git commit"
+alias gca="git commit --amend"
+alias gp="git pull --rebase"
+alias gd="git diff"
+alias gcd="git diff --cached"
+alias gl="git log"
+alias gcp="git cherry-pick"
+alias dc="docker-compose"
